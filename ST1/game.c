0de05c0a8aa725c397b9989199ea86184ed77280
TK7-PX <tksilvz7@gmail.com>
2025-08-11 01:24:44 -0300

    Add files via upload

---
+++ ST1/game.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+//estrutura quq armazena dados das cartas
+struct Carta
+{
+    char Codigo [5];
+    char Estado;
+    char Nome [15];
+    float Populacao;
+    float Area;
+    double PIB;
+    int PontosTuristicos;
+
+};
+
+int main() {
+    //criando cartas
+    struct Carta cartas[2] = {
+    {"A01",'A', "Florianópolis", 537.211, 674.844, 21.31244673, 50}, 
+    {"B01", 'B', "Manaus", 2.279686, 11.401092, 103.281436041, 30}
+    };
+    
+ char CodigoCarta[5];
+ printf("Digite o código da carta: ");
+ scanf("%s", CodigoCarta);
+
+ int achou = 0;
+
+    // Procurar pela carta
+    for (int i = 0; i < 4; i++) {
+        if (strcmp(cartas[i].Codigo, CodigoCarta) == 0) {
+            printf("\nCarta encontrada:\n");
+            printf("estado: %c\n", cartas[i].Estado);
+            printf("nome: %s\n", cartas[i].Nome);
+            printf("População: %f\n", cartas[i].Populacao);
+            printf("área: %f\n", cartas[i].Area);
+            printf("pib: %f\n", cartas[i].PIB);
+            printf("Pontos Turísticos: %d\n", cartas[i].PontosTuristicos);
+            achou = 1;
+            break;
+        }
+    }
+
+    if (!achou) {
+        printf("Código não encontrado.\n");
+    }
+
+ return 0;
+}
